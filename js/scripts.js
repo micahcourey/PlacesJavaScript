3f9287e46b71f3396b5b9944d72d2039f967a2f8
micahcourey <micahjcourey@gmail.com>
2015-09-14 16:29:12 -0700

    add to scripts.js and html file

--- js/scripts.js
+++ js/scripts.js
@@ -2,28 +2,27 @@ $(document).ready(function() {
   $("form#new-place").submit(function(event) {
     event.preventDefault();
 
-    var inputtedLocation = $("input.new-location").val();
+    var inputtedLocation = $("input#new-location").val();
     var inputtedLandmarks = $("input#new-landmarks").val();
     var inputtedTime = $("input#new-time").val();
     var inputtedNotes = $("input#new-notes").val();
 
-    var newPlace = { location: inputtedLocation, landmarks: inputtedLandmarks, time: inputtedTime, notes: inputtedNotes };
+    var newPlace = { placeLocation: inputtedLocation, landmarks: inputtedLandmarks, placeTime: inputtedTime, notes: inputtedNotes };
 
-    $(".ul#places").append("<li><span> class='place'>" + newPlace.location + "</span></li>");
-
-    $(".place").last().click(function() {
-      $("#show-place").show();
-
-      $("#show-place h2").text(newPlace.location);
-      $(".location").text(newPlace.location);
-      $(".landmarks").text(newPlace.landmarks);
-      $(".time").text(newPlace.time);
-      $(".notes").text(newPlace.notes);
-    });
+    $("ul#places").append("<li><span class='place'>" + newPlace.placeLocation + "</span></li>");
 
     $("input#new-location").val("");
     $("input#new-landmarks").val("");
     $("input#new-time").val("");
     $("input#new-notes").val("");
+
+    $(".place").last().click(function() {
+      $("#show-place").show();
+      $("#show-place h2").text(newPlace.placeLocation);
+      $(".location").text(newPlace.placeLocation);
+      $(".landmarks").text(newPlace.landmarks);
+      $(".time").text(newPlace.placeTime);
+      $(".notes").text(newPlace.notes);
+    });
   });
 });
